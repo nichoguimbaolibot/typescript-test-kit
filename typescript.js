9c7853dec77b5e2a937e2e2b8fabc5fe3368f8ee
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-07 00:20:29 +0800

    added tuple of array object, array, and object

--- typescript.js
+++ typescript.js
@@ -6,4 +6,44 @@ var string = function (a, b) {
 };
 var sumAnswer = sum1(1, 2);
 console.log('string answer: ', string("test" + sumAnswer, ' hahaha'));
-console.log(sumAnswer);
+// using of typescript according to data types
+// boolean
+var isCool = true;
+// number
+var age = 69;
+// string
+var eyeColor = 'brown';
+// string using template literals
+var favouriteQuote = "I'm not young, i'm " + age;
+// Array of string
+var animals = ['cat', 'elephant', 'jaguar', eyeColor, favouriteQuote];
+var names = [{
+        name: 'test',
+        age: 23,
+        email: 'test@gmail.com'
+    }
+];
+// types of array object static typing tuple
+var names1 = [
+    {
+        test: 'app',
+        numbers: 1,
+        booleans: true
+    },
+];
+console.log('arrayObject: ', names1);
+// object static typing tuple
+var car = {
+    brand: 'chevrolet',
+    plateNum: 9999,
+    color: 'red',
+    isManual: true
+};
+console.log('object: ', car);
+// undefined and null static typing
+var nothing = undefined;
+var impossible = null;
+// tuple array
+var arrays = [
+    'cars', 1, false
+];
--- typescript.ts
+++ typescript.ts
@@ -1,5 +1,5 @@
 const sum1 = (a: number, b: number): number => {
- return a + b; 
+ return a + b;
 }
 
 const string = (a: string, b: string): string => {
@@ -10,4 +10,65 @@ const sumAnswer = sum1(1, 2)
 console.log('string answer: ', string(`test${sumAnswer}`, ' hahaha'))
 
 
-console.log(sumAnswer)
+
+// using of typescript according to data types
+// boolean
+let isCool: boolean = true;
+// number
+let age: number = 69;
+// string
+let eyeColor: string = 'brown';
+// string using template literals
+let favouriteQuote: string = `I'm not young, i'm ${age}`
+// Array of string
+let animals: string[] = ['cat', 'elephant', 'jaguar', eyeColor, favouriteQuote];
+// Array of object
+interface objectLayout {
+  name: string,
+  age: number,
+  email: string,
+}
+let names: {name: string, age: number, email: string}[] = [{
+    name: 'test',
+    age: 23,
+    email: 'test@gmail.com'
+  }
+]
+
+interface arrayObjectLayout {
+  test: string,
+  numbers: number,
+  booleans: boolean
+}
+// types of array object static typing tuple
+let names1: Array<arrayObjectLayout> = [
+  {
+    test: 'app',
+    numbers: 1,
+    booleans: true,
+  },
+]
+
+console.log('arrayObject: ', names1)
+// object static typing tuple
+let car: {brand: string, plateNum: number, color: string, isManual: boolean} = {
+  brand: 'chevrolet',
+  plateNum: 9999,
+  color: 'red',
+  isManual: true,
+}
+
+console.log('object: ', car)
+
+// undefined and null static typing
+
+let nothing: undefined = undefined
+
+let impossible: null = null;
+
+
+// tuple array
+
+let arrays: [string, number, boolean] = [
+  'cars', 1, false
+]
